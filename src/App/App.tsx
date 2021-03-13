bb8e1b856f9a3e4acb7368d34a18ba48e03b690b
PaykVR <paykko@protonmail.com>
2021-03-13 18:40:33 +0100

    cleanups

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -5,8 +5,6 @@ import { Layout, Button, Card, Row, Col } from 'antd';
 
 const { Header, Footer, Content } = Layout;
 
-
-
 function App() {
   return (
     <div className="App">
@@ -18,40 +16,33 @@ function App() {
         </Header>
         
         <Content>
+          <Row>
+            <Col span={8}></Col>
+            <Col span={8}>
+              <Card style={{ display: 'inline-block', width: '100%', maxWidth: 300, margin:'15px', boxShadow: '7px 7px 0px 0px #EBEBEB' }}>
+                <h1>
+                  VR Demos designed for Oculus Quest 2
+                </h1>
 
-        <Row>
-          <Col span={8}></Col>
-          <Col span={8}>
-            <Card style={{ display: 'inline-block', width: '100%', maxWidth: 300, margin:'15px', boxShadow: '7px 7px 0px 0px #EBEBEB' }}>
-              <h1>
-                VR Demos designed for Oculus Quest 2
-              </h1>
+                <Button type="primary">Start VR</Button>
+              </Card>
 
-              <Button type="primary">Start VR</Button>
-            </Card>
+              <Card style={{ display: 'inline-block', width: '100%', maxWidth: 300, margin:'15px', boxShadow: '7px 7px 0px 0px #EBEBEB' }}>
+                <h1>
+                  360 Demos designed mobile phones, tablets ect
+                </h1>
 
-            <Card style={{ display: 'inline-block', width: '100%', maxWidth: 300, margin:'15px', boxShadow: '7px 7px 0px 0px #EBEBEB' }}>
-              <h1>
-                360 Demos designed mobile phones, tablets ect
-              </h1>
-
-              <Button type="primary">Start 360</Button>
-            </Card>
-          </Col>
-          <Col span={8}></Col>
-        </Row>
-
-
-
+                <Button type="primary">Start 360</Button>
+              </Card>
+            </Col>
+            <Col span={8}></Col>
+          </Row>
         </Content>
 
         <Footer>
           Some disclaimer stuff
         </Footer>
       </Layout>
-
-
-      
     </div>
   );
 }
